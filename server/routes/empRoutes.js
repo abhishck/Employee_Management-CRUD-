f0edd40da4e7563dc49f8a63ece73016ad657a32
Abhishek Rathore <abhishekrathore94500@gmail.com>
2026-02-09 23:53:04 +0530

    roles base access

--- server/routes/empRoutes.js
+++ server/routes/empRoutes.js
@@ -1,5 +1,6 @@
 import express from "express"
 import { addEmployee, deleteEmployee, getEmployeeById, getEmployees, updateEmployee } from "../controllers/empController.js";
+import authorizeRoles from "../middlewares/authorizeRole.js";
 
 const router=express.Router();
 
@@ -7,6 +8,6 @@ router.post("/",addEmployee)
 router.get("/",getEmployees)
 router.get("/:id",getEmployeeById)
 router.put("/:id",updateEmployee)
-router.delete("/:id",deleteEmployee)
+router.delete("/:id",authorizeRoles("admin"),deleteEmployee)
 
 export default router;
